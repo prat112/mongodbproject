3292813cfcced409d34a86830b1983a64f24756a
prat112 <kundapurprathiksha@gmail.com>
2023-12-03 19:40:20 +0530

    task3

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,10 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product=new Product(title,imageUrl,price,description);
+  const product=new Product(title,imageUrl,price,description,null,req.user._id);
   product
     .save()
     .then(result => {
-      // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
--- models/user.js
+++ models/user.js
@@ -1,16 +1,27 @@
-const Sequelize = require('sequelize');
+const mongoDb=require('mongodb');
+const getdb=require('../util/database').getDb;
 
-const sequelize = require('../util/database');
+const ObjectId=mongoDb.ObjectId;
+class User{
+    constructor(username,email){
+        this.name=username;
+        this.email=email;
+    }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+    save(){
+        const db=getdb();
+        return db.collection('users').insertOne(this);
+    }
 
+    static findById(userId){
+      const db=getdb();
+      return db.collection('users').findOne({_id:new ObjectId(userId)})
+        .then(user=>{
+            // console.log(user);
+            return user;  
+          })  
+        .catch(err=>console.log(err));
+
+    }
+}
 module.exports = User;
