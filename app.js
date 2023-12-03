f2d7d7dece0149e661f9517b0d65856b8b9463e1
prat112 <kundapurprathiksha@gmail.com>
2023-12-03 21:45:15 +0530

    task9

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-// const User=require('./models/user');
+const User=require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('64bf9c0e692d5fff1df232a3')
-//     .then(user => {
-//       req.user = new User(user.name,user.email,user.cart,user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('64c10a25404ed2d10353b872')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,9 +34,21 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PWD}@cluster0.qcghe17.mongodb.net/shop?retryWrites=true&w=majority`)
-    .then((result)=>{
-      app.listen(3000);
-      console.log('connected!');
-    })
+.then(result => {
+  User.findOne().then(user => {
+    if (!user) {
+      const user = new User({
+        name: 'bhush',
+        email: 'kundapurprathiksha@gmail.com',
+        cart: {
+          items: []
+        }
+      });
+      user.save();
+    }
+  });
+  app.listen(3000);
+  console.log('Connected!');
+})
     .catch(err=>console.log(err));
   
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product=new Product({title:title,description:description,price:price,imageUrl:imageUrl});
+  const product=new Product({title:title,description:description,price:price,imageUrl:imageUrl,userId:req.user});
   product
     .save()
     .then(result => {
