5bbc1ca22403cb1c4bdb8d183d8fc83b531582ea
prat112 <kundapurprathiksha@gmail.com>
2023-12-03 19:48:57 +0530

    task4

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('64bf9c0e692d5fff1df232a3')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name,user.email,user.cart,user._id);
       next();
     })
     .catch(err => console.log(err));
